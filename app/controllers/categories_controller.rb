2530f2b9524d0d5e78fac7279bfb32f99fea0a4b
Chris Chiu <hungyichiu@gmail.com>
2018-01-27 17:53:23 +0800

    user on index page can select restaurants by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.all #列出所有分類
+    @category = Category.find(params[:id]) #對應到特定分類 
+    @restaurants = @category.restaurants.page(params[:id]).per(9) #透過關聯列出選取類別的餐廳
+   
+  end
 end
