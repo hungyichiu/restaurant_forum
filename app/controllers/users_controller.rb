59590a08fd6aa8aabdbc7e128a6715160b72d4e8
Chris Chiu <hungyichiu@gmail.com>
2018-04-17 20:13:50 +0800

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update]
+
+def show
+     
+end
+
+def edit
+  
+end
+
+def update
+  if @user.update(user_params)
+    flash[:notice] = "successfuly update user profile"
+    redirect_to user_path(@user) 
+    
+
+  else
+    flash[:notice] = "Failed to update user profile"
+    render :edit
+    
+  end
+
+  
+end
+
+private
+  def set_user
+    @user = User.find(params[:id]) 
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+
+
 end
--- db/migrate/20180406025342_add.rb
+++ db/migrate/20180406025342_add.rb
@@ -5,3 +5,8 @@ class Add < ActiveRecord::Migration[5.1]
     add_column :users, :avatar, :string
   end
 end
+
+
+# 需修改 rake dev:fake_user
+# restaurant#show 評論者名稱
+# Devise 註冊表單的設計需手動修改
